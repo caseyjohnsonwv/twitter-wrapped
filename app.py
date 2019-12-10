b2f805b2ea434d374b42c535ba06102b811b0190
caseyjohnsonwv <ctj0001@mix.wvu.edu>
2019-12-10 13:17:32 -0500

    oauth1 needed

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+"""APP SETUP"""
+
 import env
 from flask import Flask, redirect, request, render_template
 from flask import session as flasksession
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import tweepy
+
 
 app = Flask(__name__)
 app.config.update(
@@ -11,7 +15,49 @@ app.config.update(
 )
 db = SQLAlchemy(app)
 
+
+"""DB MODEL(S)"""
+
+class Tweet(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
+    timestamp = db.Column(db.DateTime(), primary_key=True)
+    num_likes = db.Column(db.Integer(), default=0)
+    num_retweets = db.Column(db.Integer(), default=0)
+    def __repr__():
+        return "<Tweet by {} at {}>".format(self.username, self.timestamp)
+
+
+"""SUPPORTING FUNCTIONS"""
+
+def getApiInstance():
+    """
+    returns: tweepy api object authenticated with oauth2
+    """
+    auth = tweepy.AppAuthHandler(env.TWITTER_API_KEY, env.TWITTER_API_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+def getTopTweets(username):
+    """
+    username: str -> screen name of a twitter user
+    returns: dictionary containing 'mostRts' and 'mostLikes' with lists of 5 tweet objects each, sorted from most to fewest
+    """
+    api = getApiInstance()
+
+
+"""FLASK ROUTES"""
+
 @app.route('/', methods=['GET'])
 def home():
-    data = {'pageName':'HOME'}
+    pageName = 'HOME'
+    data = {'pageName':pageName}
+    return render_template('index.html', data=data)
+
+@app.route('/top', methods=['POST'])
+def top():
+    tweets = twitter.getTopTweets()
+    mostRts = tweets['mostRts']
+    mostLikes = tweets['mostLikes']
+    pageName = 'TOP'
+    data = {'mostRts':mostRts, 'mostLikes':mostLikes, 'pageName':pageName}
     return render_template('index.html', data=data)
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
+DATABASE_URL = os.environ['DATABASE_URL']
+TWITTER_API_KEY = os.environ['TWITTER_API_KEY']
+TWITTER_API_SECRET = os.environ['TWITTER_API_SECRET']
